1b967155c0ec0a0cf1c26ed67abb289df471a861
KATC14 <31497908+KATC14@users.noreply.github.com>
2020-01-04 18:53:28 -0500

    Update useful_things.js

--- useful_things.js
+++ useful_things.js
@@ -62,3 +62,13 @@ function checker(value, prohibited, ra) {
     return notarray;
   }
 }
+
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
