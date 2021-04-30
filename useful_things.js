6d2f2d091653f8b900f98faab4dd8a93f4234729
KATC14 <31497908+KATC14@users.noreply.github.com>
2021-04-30 10:17:37 -0400

    Update useful_things.js

--- useful_things.js
+++ useful_things.js
@@ -1,8 +1,22 @@
 String.prototype.format = function () {
-  var i = 0, args = arguments;
-  return this.replace(/{}/g, function () {
-    return typeof args[i] != 'undefined' ? args[i++] : '';
-  });
+  var args = arguments;
+  this.unkeyed_index = 0;
+  return this.replace(/\{(\w*)\}/g, function(match, key) { 
+    if (key === '') {
+      key = this.unkeyed_index;
+      this.unkeyed_index++
+    }
+    if (key == +key) {
+      return args[key] !== 'undefined' ? args[key] : match;
+    } else {
+      for (var i = 0; i < args.length; i++) {
+        if (typeof args[i] === 'object' && typeof args[i][key] !== 'undefined') {
+          return args[i][key];
+        }
+      }
+      return match;
+    }
+  }.bind(this));
 };
 
 function addGlobalStyle(css) {
